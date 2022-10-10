404c343d838955031e4a76f7220e99bfcc3e9b04
Dong Nguyen <nvdong1992@gmail.com>
2022-10-10 20:02:25 +0700

    Update template

--- backend/tools/templates/module/__module__.controller.ts
+++ backend/tools/templates/module/__module__.controller.ts
@@ -2,7 +2,7 @@ import { celebrate } from 'celebrate';
 import { NextFunction, Request, Response, Router } from 'express';
 import IController from '@/interfaces/controller.interface';
 import __module__Service from '@/modules/__module__(lowerCase)/__module__(lowerCase).service';
-import ApplicationResponse from '@/utils/response';
+import ResponseFactory from '@/utils/response';
 
 class __module__Controller implements IController {
   public path = '/__module__(lowerCase)s';
@@ -15,8 +15,6 @@ class __module__Controller implements IController {
 
   private initialiseRoutes(): void {
   }
-
- 
 }
 
 export default __module__Controller;
--- backend/tools/templates/module/__module__.spec.ts
+++ backend/tools/templates/module/__module__.spec.ts
@@ -1,6 +1,5 @@
-const sum = (a: number, b: number) => a + b;
-
 describe('api/v1/__module__(lowerCase)', () => {
+  const sum = (a: number, b: number) => a + b;
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
